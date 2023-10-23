f59df0da77562f1b1bae2f6b47b016b2138dd670
verleihernix <123760140+verleihernix@users.noreply.github.com>
2023-10-23 19:16:16 +0200

    Update install.js

--- install.js
+++ install.js
@@ -1,3 +1,13 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EasyBot = exports.EasyBotEmitter = exports.easybotemitter = exports.LevelSystem = exports.LevelXPManager = exports.TotalLevelXPManager = exports.LevelSettingsManager = exports.LevelUtilsManager = exports.LevelRanksManager = exports.LevelManager = exports.LevelDatabaseManager = exports.LevelDotParser = exports.LevelFetchManager = exports.LevelingErros = exports.LevelSettingsArray = exports.LevelDefaultOptions = exports.LevelDefaultObject = exports.LevelingError = exports.LevelEmitter = exports.levelemitter = exports.CustomLogger = exports.ErrorHandler = exports.CogManager = exports.reply = exports.DataBaseManager = exports.DiscordDataBase = exports.SlashCommandLoader = exports.SlashCommandOptionTypes = exports.MemoryGame = exports.Approve = exports.shuffleArray = exports.getAlphaEmoji = exports.oppDirection = exports.move = exports.decode = exports.formatMessage = exports.getNumEmoji = exports.disableButtons = exports.BBuilder = exports.Intents = exports.Msg = void 0;
+const discord_js_1 = require("discord.js");
+Object.defineProperty(exports, "Intents", { enumerable: true, get: function () { return discord_js_1.GatewayIntentBits; } });
+const events_1 = require("events");
+var discord_js_2 = require("discord.js");
+Object.defineProperty(exports, "Msg", { enumerable: true, get: function () { return discord_js_2.Message; } });
+const fs = require("fs");
+const fs_1 = require("fs");
+
 const colors = {
     green: '\x1b[32m',
     cyan: '\x1b[36m',
