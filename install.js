641b95780b032ecde1dc42e6edc97e5733be0687
verleihernix <123760140+verleihernix@users.noreply.github.com>
2023-10-23 19:15:21 +0200

    Create install.js

---
+++ install.js
@@ -0,0 +1,16 @@
+const colors = {
+    green: '\x1b[32m',
+    cyan: '\x1b[36m',
+    blue: '\x1b[34m'
+}
+
+console.log()
+console.log(`${colors.green}╔═══════════════════════════════════════════════════════════════════╗`)
+console.log(`${colors.green}║ @ discord-utilies                                  - [] X ║`)
+console.log(`${colors.green}║═══════════════════════════════════════════════════════════════════║`)
+console.log(`${colors.green}║ ${colors.cyan}Thank you for installing Discord Utilies!                  ${colors.green}║`)
+console.log(`${colors.green}║═══════════════════════════════════════════════════════════════════║`)
+console.log(`${colors.green}║ If you have any questions or need help, join the Support Server:  ${colors.green}║`)
+console.log(`${colors.green}║ ${colors.blue}https://discord.gg/xw5CyEPfPA                                   ${colors.green}║`)
+console.log(`${colors.green}╚═══════════════════════════════════════════════════════════════════╝\x1b[37m`)
+console.log()
